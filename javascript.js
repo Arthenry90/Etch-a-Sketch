8ef56ed3c7b91170cdcc74a3b6e8bd64be09b65e
arthenry90 <134339241+Arthenry90@users.noreply.github.com>
2024-11-24 23:07:17 -0300

    initial commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function createSquare(){
+    const square = document.createElement("div");
+    square.classList.add("square")
+    square.style.color = "white";
+    document.getElementById("mesh").appendChild(square);
+}
