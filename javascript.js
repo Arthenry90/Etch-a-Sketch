3a8a7f72648d83a1904232675c5d4807298665ce
arthenry90 <134339241+Arthenry90@users.noreply.github.com>
2024-11-27 20:35:55 -0300

    Work change color

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
 function createSquare(){
+    for(let i = 0; i < 10000;i++){
     const square = document.createElement("div");
     square.classList.add("square")
-    square.style.color = "white";
+    square.addEventListener("click", function(){
+        square.style.backgroundColor = "black";
+        console.log("clicou");
+    })
     document.getElementById("mesh").appendChild(square);
+    }
 }
+
+
+
+
