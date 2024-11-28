d56320e7f132ddd48f3e1cacbc8435d6783c8327
arthenry90 <134339241+Arthenry90@users.noreply.github.com>
2024-11-27 21:51:54 -0300

    Add reset button and color to square

--- javascript.js
+++ javascript.js
@@ -1,14 +1,29 @@
-function createSquare(){
-    for(let i = 0; i < 10000;i++){
+function removeAll(){
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(squares => squares.remove());
+}
+
+function createSquare(size){
+    for(let i = 0; i < size;i++){
     const square = document.createElement("div");
     square.classList.add("square")
-    square.addEventListener("click", function(){
-        square.style.backgroundColor = "black";
+    square.addEventListener("mouseout", function(){
+        square.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
         console.log("clicou");
     })
     document.getElementById("mesh").appendChild(square);
     }
 }
+window.onload = createSquare(4096);
+
+document.getElementById("btn").addEventListener("click", function(){
+    const tamanho = prompt("Selecione o tamanho");
+    removeAll();
+    console.log(tamanho);
+    createSquare(tamanho * tamanho);
+});
+
+
 
 
 
